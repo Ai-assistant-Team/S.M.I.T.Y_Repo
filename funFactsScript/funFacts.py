049e36af24e3a6db65e02cb700fda61a24933969
manolispolymeneris <81076935+manolispolymeneris@users.noreply.github.com>
2021-03-28 09:52:29 +0300

    Add files via upload

---
+++ funFactsScript/funFacts.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 27 13:08:39 2021
+
+@author: manolispolymeneris
+"""
+
+def funFacts():
+    import randfacts #Imports random fun fact generator library
+    
+    for i in range(1486): #Runs the list with fun facts
+        fact = randfacts.getFact() #Generates a fun fact
+        print(fact) #Prints the fun fact
+        
+        request = input("Do you want to see another Fun Fact? Yes/No: ") #Asks user if we wants another fun fact
+        
+        if (request!="Yes" and request!="No"): #Checks if answer isn't "Yes" or "No"
+            print("Invalid Answer") #Prints that the answer is not valid
+            break
+        #end of if
+        if (request=="No"): #Checks if user wants another fun fact
+            break
+        #end of if
+    #end of for
+        
+funFacts()
