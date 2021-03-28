262d4a90343be9f85a869bcfd4268a105c3f856f
Teo Economides <teo.economides.vodafone@gmail.com>
2021-03-28 07:11:18 +0300

    Update openUrls.py

--- googleMapsScripts/openUrls.py
+++ googleMapsScripts/openUrls.py
@@ -6,6 +6,7 @@ Created on Tue Mar 23 08:45:24 2021
 """
 
 def openUrl(url):
+    #made by Οικονομίδης Θεόδωρος
     import webbrowser                                           # imports the needed library
     
     webbrowser.open_new(url)                                    # opens the given url, using a webbrowser
