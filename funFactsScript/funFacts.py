f376a11ee0f76038464328e9ae54a354c9c2b62f
manolispolymeneris <81076935+manolispolymeneris@users.noreply.github.com>
2021-05-05 11:30:40 +0300

    Update funFacts.py

--- funFactsScript/funFacts.py
+++ funFactsScript/funFacts.py
@@ -8,7 +8,7 @@ Created on Sat Mar 27 13:08:39 2021
 def funFacts():
     import randfacts #Imports random fun fact generator library
     
-    for i in range(1486): #Runs the list with fun facts
+    for i in range(5142): #Runs the list with fun facts
         fact = randfacts.getFact() #Generates a fun fact
         print(fact) #Prints the fun fact
         
