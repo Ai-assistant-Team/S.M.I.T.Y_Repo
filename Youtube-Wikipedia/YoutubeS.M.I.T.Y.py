53dc668d2ad4db123145b5f8c488c54dc371778c
Tsialis <80192953+Tsialis@users.noreply.github.com>
2021-05-06 13:23:20 +0300

    Update YoutubeS.M.I.T.Y.py

--- Youtube-Wikipedia/YoutubeS.M.I.T.Y.py
+++ Youtube-Wikipedia/YoutubeS.M.I.T.Y.py
@@ -5,8 +5,8 @@ import wikipedia                        # importing wikipedia module
 
 listener = sr.Recognizer()                      # recognizer to recognize your voice
 engine = pyttsx3.init()                         # pyttsx3 is a text-to-speech conversion library in Python
-voices = engine.getProperty("voices")           # making a male voice sound female
-engine.setProperty("voice", voices[2].id)       # making a male voice sound female
+voices = engine.getProperty('voices')           # making a male voice sound female
+engine.setProperty('voice', voices[2].id)       # making a male voice sound female
 
 
 ##
@@ -26,16 +26,16 @@ def talk(text):             # declaring a function and every time we call this s
 def take_command():                                                     # declaring a function
     try:                                                                # incase microphone not work
         with sr.Microphone() as source:                                 # use microphone and give name(source)
-            print("Listening...")                                       # printing Listening
-            talk("Listening...")                                        # saying she is listening
+            print('Listening...')                                       # printing Listening
+            talk('Listening...')                                        # saying she is listening
             voice = listener.listen(source)                             # listen to this source
             command = listener.recognize_google(voice)                  # provides access to the speech recognition service
             command = command.lower()                                   # Python string method lower() returns a copy of the string in which all case-based characters have been lowercased.
-            if "smity" in command:                                      # working only if she hears her name(smity)
-                command = command.replace("smity", "")                  # replacing word smity with blank
+            if 'smity' in command:                                      # working only if she hears her name(smity)
+                command = command.replace('smity', '')                  # replacing word smity with blank
                 talk(command)                                           # if "smity" in command she will talk
             elif "who is " in command:                                  # giving wikipedia info if she hears the sentence"who is"
-                person = command.replace("who is ", "")                 # replacing "who is" with blank
+                person = command.replace('who is ', '')                 # replacing "who is" with blank
                 info = wikipedia.summary(person, 3)                     # recognizing wikipedia info and giving 3 lines answer
                 print(info)                                             # printing info
                 talk(info)                                              # saying info
@@ -51,9 +51,9 @@ def take_command():                                                     # declar
 
 def run_smity():                                            # declaring a function
     command = take_command()                                # taking a command from user
-    if "play" in command:                                   # playing only if she hears the word play
-        song = command.replace("play", "")                  # replacing the word play with blank
-        talk("playing" + song)                              # she says that she is playing and the song name
+    if 'play' in command:                                   # playing only if she hears the word play
+        song = command.replace('play', '')                  # replacing the word play with blank
+        talk('playing' + song)                              # she says that she is playing and the song name
         pywhatkit.playonyt(song)                            # opening youtube and playing what user said
                                                             # end of <run_smity/Structure>
 
