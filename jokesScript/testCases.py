12cf1c31b175faeaa62fd242462b16c8dd167477
manolispolymeneris <81076935+manolispolymeneris@users.noreply.github.com>
2021-05-06 12:50:01 +0300

    Update testCases.py

--- jokesScript/testCases.py
+++ jokesScript/testCases.py
@@ -1,44 +1,28 @@
 """
-Created on We May 5 18:07:19 2021
+Created on We May 5 18:03:12 2021
 @author: manolispolymeneris
 """
 
+import pyjokes
+import jokes
+
 def testCaseOne():
-    import pyjokes #Imports random joke generator library
     
-    for i in range(9, 105): #Runs the list with jokes
-        joke = pyjokes.get_joke() #Generates a joke
-        print(joke) #Prints the joke
-        
-        request = "Yes" #Sets request to "Yes"
-        
-        if (request!="Yes" and request!="No"): #Checks if answer isn't "Yes" or "No"
-            print("Invalid Answer") #Prints that the answer is not valid
-            break
-        #end of if
-        if (request=="No"): #Checks if user wants another joke
-            break
-        #end of if
-    #end of for
-        
+    request='Yes'
+    jokes(request)
+    
 testCaseOne()
 
 def testCaseTwo():
-    import pyjokes #Imports random joke generator library
     
-    for i in range(9, 105): #Runs the list with jokes
-        joke = pyjokes.get_joke() #Generates a joke
-        print(joke) #Prints the joke
-        
-        request = "No" #Sets request to "No"
-        
-        if (request!="Yes" and request!="No"): #Checks if answer isn't "Yes" or "No"
-            print("Invalid Answer") #Prints that the answer is not valid
-            break
-        #end of if
-        if (request=="No"): #Checks if user wants another joke
-            break
-        #end of if
-    #end of for
-        
+    request='No'
+    jokes(request)    
+    
 testCaseTwo()
+
+def testCaseThree():
+    
+    request='Maybe'
+    jokes(request)    
+    
+testCaseThree()
