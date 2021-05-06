ffbaa150a4a9b6e5b6f98951827a163a882d871e
manolispolymeneris <81076935+manolispolymeneris@users.noreply.github.com>
2021-05-06 12:45:07 +0300

    Update testCases.py

--- funFactsScript/testCases.py
+++ funFactsScript/testCases.py
@@ -3,42 +3,17 @@ Created on We May 5 18:03:12 2021
 @author: manolispolymeneris
 """
 
+import randfacts
+import funFacts
+
 def testCaseOne():
-    import randfacts #Imports random fun fact generator library
     
-    for i in range(5142): #Runs the list with fun facts
-        fact = randfacts.getFact() #Generates a fun fact
-        print(fact) #Prints the fun fact
-        
-        request = "Yes" #Sets request to "Yes"
-        
-        if (request!="Yes" and request!="No"): #Checks if answer isn't "Yes" or "No"
-            print("Invalid Answer") #Prints that the answer is not valid
-            break
-        #end of if
-        if (request=="No"): #Checks if user wants another fun fact
-            break
-        #end of if
-    #end of for
-        
+    funFacts()  
+    
 testCaseOne()
 
 def testCaseTwo():
-    import randfacts #Imports random fun fact generator library
     
-    for i in range(5142): #Runs the list with fun facts
-        fact = randfacts.getFact() #Generates a fun fact
-        print(fact) #Prints the fun fact
-        
-        request = "No" #Sets request to "No"
-        
-        if (request!="Yes" and request!="No"): #Checks if answer isn't "Yes" or "No"
-            print("Invalid Answer") #Prints that the answer is not valid
-            break
-        #end of if
-        if (request=="No"): #Checks if user wants another fun fact
-            break
-        #end of if
-    #end of for
-        
+    funFacts()    
+    
 testCaseTwo()
