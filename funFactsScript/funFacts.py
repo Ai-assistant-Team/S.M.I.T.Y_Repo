0d1173c5f277423bd4fdc917000d36e78c620a11
manolispolymeneris <81076935+manolispolymeneris@users.noreply.github.com>
2021-05-06 12:57:41 +0300

    Update funFacts.py

--- funFactsScript/funFacts.py
+++ funFactsScript/funFacts.py
@@ -12,15 +12,4 @@ def funFacts():
         fact = randfacts.getFact() #Generates a fun fact
         print(fact) #Prints the fun fact
         
-        request = input("Do you want to see another Fun Fact? Yes/No: ") #Asks user if we wants another fun fact
-        
-        if (request!="Yes" and request!="No"): #Checks if answer isn't "Yes" or "No"
-            print("Invalid Answer") #Prints that the answer is not valid
-            break
-        #end of if
-        if (request=="No"): #Checks if user wants another fun fact
-            break
-        #end of if
-    #end of for
-        
 funFacts()
