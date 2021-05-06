6006d5919c146f11f604b9fd1c0ad5b6f637c2f6
manolispolymeneris <81076935+manolispolymeneris@users.noreply.github.com>
2021-05-06 12:19:18 +0300

    Update jokes.py

--- jokesScript/jokes.py
+++ jokesScript/jokes.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Mar 27 13:08:39 2021
 
 @author: manolispolymeneris
 """
 
+import pyjokes #Imports random joke generator library
+
 def jokes():
-    import pyjokes #Imports random joke generator library
     
     for i in range(9, 105): #Runs the list with jokes
         joke = pyjokes.get_joke() #Generates a joke
