79e7929175bacd85ff5502cb8c5074b337d690bd
GeorgiosGew <80193108+GeorgiosGew@users.noreply.github.com>
2021-05-07 13:15:26 +0300

    Update photoCapture.py

--- PhotoCaptureScript/photoCapture.py
+++ PhotoCaptureScript/photoCapture.py
@@ -1,7 +1,7 @@
 from ecapture import ecapture as ec
 #import the parts of the ecapture that are needed
 
-def test():
+def capture():
 
     ec.capture(0,"robo camera","img.jpg")
 
@@ -13,5 +13,5 @@ def test():
 
 #Save name (It can be a variable or a string. If you don't wish to save the image, type False)
 
-test()
+capture()
 
