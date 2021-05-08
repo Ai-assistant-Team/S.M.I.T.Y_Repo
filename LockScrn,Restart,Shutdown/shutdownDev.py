d0c38e6030793ac5b7fefe3bf169c610b8f74be1
NickKoutamanis <81077029+NickKoutamanis@users.noreply.github.com>
2021-05-08 09:51:56 +0300

    Update shutdownDev.py

--- LockScrn,Restart,Shutdown/shutdownDev.py
+++ LockScrn,Restart,Shutdown/shutdownDev.py
@@ -6,3 +6,12 @@ def shutdownDev():
     os.system("shutdown /s /t 1") #This method is used to shut down the device.
                                   #/s is for shut down and /t is a timer so that the system shuts down after 1 sec.
     
+#This fnction is used to shut down the device after a number of seconds speciafied by the user
+def shutdownDevTimer(t):
+    import time
+    
+    while(t>0):
+        time.sleep(1)
+        t = int(t)-1;
+    
+    shutdownDev()
