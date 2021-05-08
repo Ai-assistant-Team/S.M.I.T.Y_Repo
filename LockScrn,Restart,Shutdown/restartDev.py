dedda9055482a4e1983437d86956aa1fb9e0fbfe
NickKoutamanis <81077029+NickKoutamanis@users.noreply.github.com>
2021-05-08 09:50:47 +0300

    Update restartDev.py

--- LockScrn,Restart,Shutdown/restartDev.py
+++ LockScrn,Restart,Shutdown/restartDev.py
@@ -6,4 +6,12 @@ def restartDev():
     os.system("shutdown /r /t 1")#This method is used to shut down the device.
                                   #/r is for restart and /t is a timer so that the system shuts down after 1 sec.
         
-restartDev()
+#This fnction is used to restart the device after a number of seconds speciafied by the user
+def restartDevTimer(t):
+    import time
+    
+    while(t>0):
+        time.sleep(1)
+        t = int(t)-1;
+    
+    restartDev()        
