91214aec03271bcd880aa40e704057f9d3f623e5
Tsialis <80192953+Tsialis@users.noreply.github.com>
2021-05-10 10:54:50 +0300

    Update Youtubescript.py

--- Youtube/Youtubescript.py
+++ Youtube/Youtubescript.py
@@ -1,1 +1,11 @@
+import webbrowser                                                                                   # importing webbrowser module
 
+##
+## This function is searching youtube the video you want
+##
+
+def Youtube_Search(toSearch):                                                                       # declaring a function
+    try:                                                                                            # using Exception Handling to avoid unprecedented errors
+        return(webbrowser.open('https://www.youtube.com/results?search_query=' + toSearch))         # it searches a youtube video of what you typed
+    except:                                                                                         # printing the error message
+        print('An unknown error occured')
