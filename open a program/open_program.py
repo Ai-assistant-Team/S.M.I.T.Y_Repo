e37eeda9ce78a1550198e658c9c9cc5ff282ac5f
NektariosPapagalakis <79308500+NektariosPapagalakis@users.noreply.github.com>
2021-06-08 12:16:22 +0300

    Update open_program.py

--- open a program/open_program.py
+++ open a program/open_program.py
@@ -1,53 +1,59 @@
 import webbrowser
 import subprocess
-import os
-
-
 from program import Program
+import pathlib
 
-def open_program(program):
-    # if the type of the program is .exe
-    if program.program_type == "exe":
-        try:
-            #go to the program.locatino and start the executable file t
-            subprocess.Popen(program.location)
-        except:
-            #if ther is a problem send an error message
-            print("Problem opening " + program.name)
-    # if the type of the program is web_site       
-    elif program.program_type == "web_site":
-        #open the last used browewr and go to the url that is in the program.location
-        webbrowser.open_new_tab(p.location)
-    # if none of te above send a message
+def open_program(program_name):
+    listOfPrograms =[]
+    listOfwebsites =[]
+    execution = 0
+    execution = load(listOfPrograms,"\\program_locations.txt")
+    execution = load(listOfwebsites,"\\users_urls.txt")
+    if execution == 1:
+        return 10
     else:
-       print("There is a problem")
+        found = 'N'
+        for program in listOfPrograms:
+            if program.name == program_name:
+                print(program.name)
+                found = 'Y'
+                # if the type of the program is .exe
+                try:
+                    #go to the program.locatino and start the executable file t
+                    subprocess.Popen(program.location)
+                except:
+                    #if ther is a problem send an error message
+                    return 3
+        
+        if found == "N":
+            for website in listOfwebsites:
+                if website.name == program_name:
+                    print(website.name)
+                    found = 'Y'
+                    try:
+                        webbrowser.open_new_tab(website.location)
+                    except:
+                        return 14
+                    #open the last used browewr and go to the url that is in the program.location
+        if found == 'N':
+            return 1
+
 
 #installation of programs, adding the to the list of Programs
-def create_programs(listOfPrograms):
-    notepad = Program("notepad","exe",'C:\\Windows\\System32\\notepad.exe')
-    listOfPrograms.append(notepad)
-    calculator = Program("calculator","exe", 'C:\\Windows\\System32\\calc.exe')
-    listOfPrograms.append(calculator)
-    google = Program("google","web_site","https://www.google.com")
-    listOfPrograms.append(google)
-    facebook = Program("facebook","web_site","https://www.facebook.com")
-    listOfPrograms.append(facebook)
-    messenger = Program("messenger","exe", 'C:\\Users\\NektariosP\\AppData\\Local\\Programs\\Messenger\\Messenger.exe')
-    listOfPrograms.append(messenger)
 
-#Test  
-def test(listOfPrograms):
-    create_programs(listOfPrograms)
-    answer = input("What do you want to open?")
-    found = False
-    for x in listOfPrograms:
-        if x.name == answer:
-            found = True
-            open_program(x)
-    if found == False:
-        print("Program Not found")
-
-
-
-listOfPrograms = []
-test(listOfPrograms)
+def load(list,txt_location):
+    try:
+        location = pathlib.Path(__file__).parent.absolute()
+        f = open("%s%s"%(location,txt_location), "r")
+        while 5>4:
+            name = f.readline()
+            name = name.replace('\n','')
+            if not name:
+                break
+            program_location = f.readline()
+            program_location = program_location.replace('\n','')
+            list.append(Program(name,program_location))
+        f.close()
+        return 0
+    except:
+        return 1
