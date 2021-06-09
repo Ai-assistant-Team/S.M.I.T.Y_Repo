427c6aa8f57813551b43df4b4127c935e23d9587
Teo Economides <teo.economides.vodafone@gmail.com>
2021-06-09 12:34:11 +0300

    Add files via upload

--- googleMapsScripts/README.txt
+++ googleMapsScripts/README.txt
@@ -1,7 +1,11 @@
+
+---------- 28/03/2021 ----------
+
 __init__.py : exists so the interpreter knows that the content of this folder is importable
+
+---------- 08/05/2021 ----------
 
 googleMapsDirections.py : contains a function that opens a Google Maps Directions Page, having already filled some elements, depending on the parameters passed.
 
 googleMapsSearch.py : contains a function that opens a Google Maps Search Page. The parameter passed is used as the search content. (if no parameter is passed, then the page opens with an empty search bar)
 
-openUrls.py : contains a function that is used to open URLs, using the system's default browser. (is exists, for the purpose of avoiding to repeat the same code multiple times)
--- googleMapsScripts/googleMapsSearch.py
+++ googleMapsScripts/googleMapsSearch.py
@@ -5,34 +5,37 @@ Created on Tue Mar 23 06:11:25 2021
 @author: Θόδωρος
 """
 
-def googleMapsSearch(query=""):
+def googleMapsSearch(query=''):
     #made by Οικονομίδης Θεόδωρος
     ## If called with a parameter, it opens a google maps page and searches for the given text
     ## If called without a parameter, it opens a google maps page, with an empty search bar
     
-    from openUrls import openUrl
+    try:
+        
+        # imports
+        
+        from urlHandling.openUrls import openUrl
+        
+        from urllib.parse import urlencode, quote_plus              # import the parts of the urllib.parse that are needed 
     
-    from urllib.parse import urlencode, quote_plus              # import the parts of the urllib.parse that are needed 
-
-
-    url_base = "https://www.google.com/maps/search/?api=1"      # this is the url that leads to google maps search
+        # functionality
     
-    payload = {"query" : query}                                 # before it gets transformed into a url form, the payload must first be written as a dictionary. (The payload contains the search parameters)
+        returnCode = 0                                              # 0 : No Errors        
+
+        url_base = 'https://www.google.com/maps/search/?api=1'      # this is the url that leads to google maps search
+        
+        payload = {'query' : query}                                 # before it gets transformed into a url form, the payload must first be written as a dictionary. (The payload contains the search parameters)
+        
+        url_form_payload = urlencode(payload, quote_via=quote_plus) # convert the dictionary into url-compatible form
     
-    url_form_payload = urlencode(payload, quote_via=quote_plus) # convert the dictionary into url-compatible form
-
-    final = url_base + "&" + url_form_payload                   # concatenate the url_base with the, url-compatible, payload (a '&' is needed in between)
+        final = url_base + '&' + url_form_payload                   # concatenate the url_base with the, url-compatible, payload (a '&' is needed in between)
+        
+        returnCode = openUrl(final)                                 # opens the final url in a Web Browser and passes the returned value in <returnCode>
+        
+        return returnCode
     
-    openUrl(final)                                              # opens the final url in a Web Browser
-
-
-
-
-def test():
-    #made by Οικονομίδης Θεόδωρος
-    ## Used only for testing the code
-    googleMapsSearch(input("What would you like to search?\n"))
-    googleMapsSearch()                                          # when used without parameters, it opens google maps empty search 
+    except Exception:
+        
+        return 1
     
-    
-test()
+#end of googleMapsSearch
