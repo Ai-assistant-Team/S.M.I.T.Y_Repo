8f11098efabc87e2838c1846037a3a8c34dd3fb0
NektariosPapagalakis <79308500+NektariosPapagalakis@users.noreply.github.com>
2021-06-12 15:57:56 +0300

    Update news.py

--- News/news.py
+++ News/news.py
@@ -8,7 +8,10 @@ import webbrowser
 def get_news():
     news_titles =""
     list_of_links = []
-    list_of_links = get_list_from_txt(list_of_links)
+    try:
+        list_of_links = get_list_from_txt(list_of_links)
+    except:
+        return 8
     totall = int(list_of_links[0])
     used = int(list_of_links[1])
     start = int(list_of_links[2])
@@ -21,7 +24,10 @@ def get_news():
             step = totall-used
 
         for x in range(start,end+1):
-            news_titles = news_titles + get_title_from_article(list_of_links[x]) + '\n'
+            title_article =  get_title_from_article(list_of_links[x])
+            if title_article == 10:
+                return 10
+            news_titles = news_titles + title_article + '\n'
 
         used = used +step
         start = start +step
@@ -29,22 +35,27 @@ def get_news():
         list_of_links[1] = str(used) + '\n'
         list_of_links[2] = str(start) + '\n'
         list_of_links[3] = str(end) + '\n'
-        update_txt(list_of_links)
+        a = update_txt(list_of_links)
+        if a == 9:
+            return 9
     else:
         news_titles = "No more news aveliable"
     return news_titles
 
 def open_news_page(title):
-    list_of_links = []
-    list_of_links = get_list_from_txt(list_of_links)
-    fount = 'no'
-    end = int(list_of_links[1]) + 4
-    for x in range(4,end):
-        if (get_title_from_article(list_of_links[x]) == title):
-            fount = 'yes'
-            webbrowser.open_new_tab(list_of_links[x])
-            break
-    if fount == 'no':
-        return 'Title not found'
-    else:
-        return 'Title found'
+    try:
+        list_of_links = []
+        list_of_links = get_list_from_txt(list_of_links)
+        fount = 'no'
+        end = int(list_of_links[1]) + 4
+        for x in range(4,end):
+            if (get_title_from_article(list_of_links[x]) == title):
+                fount = 'yes'
+                webbrowser.open_new_tab(list_of_links[x])
+                break
+        if fount == 'no':
+            return 'Title not found'
+        else:
+            return 'Title found'
+    except:
+        return 10
