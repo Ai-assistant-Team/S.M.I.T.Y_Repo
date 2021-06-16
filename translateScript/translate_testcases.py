fb8d6b3997f26bfd6cbf0697291123d2039b7469
FotisVlachos <81076889+FotisVlachos@users.noreply.github.com>
2021-06-16 19:53:44 +0300

    Update and rename translateTestCase.py to translate_testcases.py

---
+++ translateScript/translate_testcases.py
@@ -0,0 +1,16 @@
+from translateText import *
+
+def testCaseOne():
+    translateText('dog','hr')
+
+def testCaseTwo():
+    translateText(0,0)
+
+def testCaseThree():
+    translateText('dog','english')
+
+def testCaseFour():
+    translateText('dog',0)
+
+def testCaseFive():
+    translateText(0,'el')
