f7d24ce96c591ac7756e7af433ee3d5f1088a4f7
dsoump <80273571+dsoump@users.noreply.github.com>
2021-06-16 23:52:01 +0300

    Update instagram_messages_count.py

--- instagram_messages/instagram_messages_count.py
+++ instagram_messages/instagram_messages_count.py
@@ -12,7 +12,7 @@ def instagram_messages_count():
 	# op.add_argument('headless')
 	driver = webdriver.Chrome(ChromeDriverManager().install(),options=op)
 
-	driver.get('https://www.instagram.com/')#Opens facebook page
+	driver.get('https://www.instagram.com/')#Opens instagram page
 
 	try:
 		element = WebDriverWait(driver,600).until(cond.element_to_be_clickable((By.CSS_SELECTOR, 'path[d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"]')))
