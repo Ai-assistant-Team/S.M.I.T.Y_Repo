ae9f97dec511ee771547b8da6d72bcd0672d6ec3
NickKoutamanis <81077029+NickKoutamanis@users.noreply.github.com>
2021-06-16 21:16:48 +0300

    Add files via upload

--- playMusicLocal/playMusicLocal.py
+++ playMusicLocal/playMusicLocal.py
@@ -10,14 +10,14 @@ def playMusicLocal(songName, songPath): #Given a name and a path plays an audio 
         import sys #Required to stop the script if audio file is not found
 
     
-        audioTypes = [".mp3",".m4a",".flac",".wav",".wma",".aac"] #The audio types supported by the script
+        audioTypes = ['.mp3','.m4a','.flac','.wav','.wma','.aac'] #The audio types supported by the script
     
         for i in range(6): 
             result = find(songName+audioTypes[i], songPath) #Searches for the audio file with every one of the above extentions   
             if result != None: #if the audio file is found break the loop
                 break   
         
-        if result == None: #if the audio file is not found return code 2 and stop script
+        if (result == None)or(result == 1): #if the audio file is not found return code 2 and stop script
             return 2
             sys.exit()
         
@@ -28,9 +28,11 @@ def playMusicLocal(songName, songPath): #Given a name and a path plays an audio 
         return 1
     
 def find(fileName, filePath): #Searches for the audio file in a given path and if found returns full path
-    import os
-    for root, dirs, files in os.walk(filePath):
-        if fileName in files:
-            return os.path.join(root, fileName)
-    
+    try:
+        import os
+        for root, dirs, files in os.walk(filePath):
+            if fileName in files:
+                return os.path.join(root, fileName)
+    except:
+        return 1
     
