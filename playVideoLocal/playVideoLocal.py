4bf376e05e2217d3c2106fbc882a8d3472c7aa41
NickKoutamanis <81077029+NickKoutamanis@users.noreply.github.com>
2021-06-16 21:20:55 +0300

    Add files via upload

--- playVideoLocal/playVideoLocal.py
+++ playVideoLocal/playVideoLocal.py
@@ -11,14 +11,14 @@ def playVideoLocal(videoName, videoPath): #Given a name and a path plays a video
         import sys #Required to stop the script if video file is not found
 
     
-        videoTypes = [".m4v",".mp4",".mov",".asf",".avi",".wmv",".m2ts",".3g2",".3gp2",".3gpp"] #The video types supported by the script
+        videoTypes = ['.m4v','.mp4','.mov','.asf','.avi','.wmv','.m2ts','.3g2','.3gp2','.3gpp'] #The video types supported by the script
     
         for i in range(6):
             result = find(videoName+videoTypes[i], videoPath) #Searches for the video file with every one of the above extentions
             if result != None: #if the video file is found break the loop
                 break
         
-        if result == None: #if the video file is not found return code 2 and stop script
+        if (result == None)or(result == 1): #if the video file is not found return code 2 and stop script
             return 2
             sys.exit()
         
@@ -29,8 +29,10 @@ def playVideoLocal(videoName, videoPath): #Given a name and a path plays a video
         return 1
     
 def find(fileName, filePath): #Searches for the video file in a given path and if found returns full path
-    import os
-    for root, dirs, files in os.walk(filePath):
-        if fileName in files:
-            return os.path.join(root, fileName)
-    
+    try:
+        import os
+        for root, dirs, files in os.walk(filePath):
+            if fileName in files:
+                return os.path.join(root, fileName)
+    except:
+        return 1
