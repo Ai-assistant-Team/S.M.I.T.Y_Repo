e3e628cd804e2a9bcedce7b1fd9323354856f13f
Dimitris Soumpasis <ics20196@uom.edu.gr>
2021-06-16 16:46:05 +0300

    fixed fb and insta def for getting msg count

--- fb_messages/facebook_messages_count.py
+++ fb_messages/facebook_messages_count.py
@@ -1,64 +1,32 @@
-print('Loading Libraries...')
-from time import sleep
+from time import sleep			#Libraries used are 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as cond
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-print('Finished loading libraries.')
+def facebook_messages_count():
+	op = webdriver.ChromeOptions() 	#Here the scipt loads the chrome browser
+	op.add_argument('--disable-notifications')
+	# op.add_argument('headless')
+	driver = webdriver.Chrome(ChromeDriverManager().install(),options=op)
 
-op = webdriver.ChromeOptions()
-#op.add_argument('headless')
-print('Loading browser...')
-driver = webdriver.Chrome('./chromedriver',options=op)
-print("Finished loading browser.")
+	driver.get('https://www.facebook.com/')#Opens facebook page
 
-location = "Miami--FL"
-category = "nature-and-outdoors"
-url = f'https://fr.airbnb.com/s/{location}/experiences/{category}'
-
-usr=''
-pwd='' 
-
-driver.get('https://www.facebook.com/')
-print ("Opened facebook")
-sleep(1)
-  
-username_box = driver.find_element_by_id('email')
-username_box.send_keys(usr)
-print ("Email Id entered")
-sleep(1)
-  
-password_box = driver.find_element_by_id('pass')
-password_box.send_keys(pwd)
-print ("Password entered")
-  
-login_box = driver.find_elements_by_tag_name('button')[0]
-login_box.click()
-
-try:
-	dates = []
-	print('waiting elements...')
-	elements = WebDriverWait(driver, 10).until(cond.visibility_of_all_elements_located((By.TAG_NAME, 'a')))
-	for e in elements:
-		print(e.get_attribute("aria-label"))
-	divs = driver.find_elements_by_tag_name('div')
-	for div in divs:
-		if div.get_attribute("aria-label") is not None:
-			if div.get_attribute('aria-label').find('Messenger') != -1:
-				print(div.get_attribute('aria-label'))
-				break
-	print('nothing')
-except:
-	divs = driver.find_elements_by_tag_name('div')
-	for div in divs:
-		if div.get_attribute("aria-label") is not None:
-			if div.get_attribute('aria-label').find('Messenger') != -1:
-				print(div.get_attribute('aria-label'))
-				break
-	print('nothing')
-	print('closed')
-		
-
-print("Finished")
+	try:
+		element = WebDriverWait(driver,600).until(cond.element_to_be_clickable((By.CSS_SELECTOR, 'path[d="M14 2.042c6.76 0 12 4.952 12 11.64S20.76 25.322 14 25.322a13.091 13.091 0 0 1-3.474-.461.956 .956 0 0 0-.641.047L7.5 25.959a.961.961 0 0 1-1.348-.849l-.065-2.134a.957.957 0 0 0-.322-.684A11.389 11.389 0 0 1 2 13.682C2 6.994 7.24 2.042 14 2.042ZM6.794 17.086a.57.57 0 0 0 .827.758l3.786-2.874a.722.722 0 0 1 .868 0l2.8 2.1a1.8 1.8 0 0 0 2.6-.481l3.525-5.592a.57.57 0 0 0-.827-.758l-3.786 2.874a.722.722 0 0 1-.868 0l-2.8-2.1a1.8 1.8 0 0 0-2.6.481Z"]')))
+		divs = driver.find_elements_by_tag_name('div')
+		try:
+			for div in divs:
+				if div.get_attribute("aria-label") is not None:
+					if div.get_attribute('aria-label').find('Messenger') != -1:
+						if(len(div.get_attribute('aria-label')) == 9):
+							return('No message found')
+						else:
+							res = [int(s) for s in div.get_attribute('aria-label').split() if s.isdigit()]
+							return("You have " + str(res[0]) + " unread message(s)")
+		except:
+			return("*1")
+	except:
+		return('timeout')
--- instagram_messages/instagram_messages_count.py
+++ instagram_messages/instagram_messages_count.py
@@ -1,52 +1,26 @@
-print('Loading Libraries...')
-from time import sleep
+from time import sleep			#Libraries used are 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as cond
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-print('Finished loading libraries.')
+def instagram_messages_count():
+	op = webdriver.ChromeOptions() 	#Here the scipt loads the chrome browser
+	op.add_argument('--disable-notifications')
+	# op.add_argument('headless')
+	driver = webdriver.Chrome(ChromeDriverManager().install(),options=op)
 
-op = webdriver.ChromeOptions()
-#op.add_argument('headless')
-print('Loading browser...')
-driver = webdriver.Chrome('./chromedriver',options=op)
-print("Finished loading browser.")
+	driver.get('https://www.instagram.com/')#Opens facebook page
 
-usr='biosoinbeauty'
-pwd='cocojumbo06' 
+	try:
+		element = WebDriverWait(driver,600).until(cond.element_to_be_clickable((By.CSS_SELECTOR, 'path[d="M47.8 3.8c-.3-.5-.8-.8-1.3-.8h-45C.9 3.1.3 3.5.1 4S0 5.2.4 5.7l15.9 15.6 5.5 22.6c.1.6.6 1 1.2 1.1h.2c.5 0 1-.3 1.3-.7l23.2-39c.4-.4.4-1 .1-1.5zM5.2 6.1h35.5L18 18.7 5.2 6.1zm18.7 33.6l-4.4-18.4L42.4 8.6 23.9 39.7z"]')))
+		try:
+			nOfMsg = driver.find_element_by_class_name('bqXJH')
+			return("You have " + nOfMsg.get_attribute('innerHTML') + " new message(s)")
+		except:
+			return("No new messages found")
+	except:
+		return('timeout')
 
-driver.get('https://www.instagram.com/')
-print ("Opened instagram")
-sleep(1)
-  
-username_box = driver.find_element_by_xpath("//input[@name='username']")
-username_box.send_keys(usr)
-print ("Email entered")
-sleep(1)
-  
-password_box = driver.find_element_by_xpath("//input[@name='password']")
-password_box.send_keys(pwd)
-password_box.send_keys(u'\ue007')
-print ("Password entered")
-
-
-try:
-	dates = []
-	print('waiting elements...')
-	elements = WebDriverWait(driver, 5).until(cond.visibility_of_all_elements_located((By.CLASS_NAME, 'Fifk5')))
-	messages = 0
-	for e in elements:
-		if e.text != "\n" and e.text != '':
-			#print("".join(e.text.split()))
-			messages = int("".join(e.text.split()))
-	print(f'{messages} messages found')
-except:
-	elements = driver.find_elements_by_tag_name('a')
-	for e in elements:
-		if e.get_attribute("aria-label") is not None:
-			if e.get_attribute("aria-label").find("Notifications,") != -1:
-				print(e.get_attribute("aria-label"))
-	print('exception')
-		
