1bf5f29afd5de20be9b4ce87508d16a0ea12b95d
NickKoutamanis <81077029+NickKoutamanis@users.noreply.github.com>
2021-06-16 21:36:02 +0300

    Add files via upload

---
+++ playVideoLocal/playVideoLocal_TestCases.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 21:25:09 2021
+
+@author: nickk
+"""
+
+import playVideoLocal
+
+playVideoLocal("Smells like teen spirit", "C://users/exaple/videos") #Correct example
+
+playVideoLocal("C://users/exaple/video") #Only path
+
+playVideoLocal("Smells like teen spirit") #Only Name
+
+playVideoLocal() #No input
