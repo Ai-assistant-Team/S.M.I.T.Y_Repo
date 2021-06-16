2475b7a268e717e630a481b93f04a18488bf6d5c
manolispolymeneris <81076935+manolispolymeneris@users.noreply.github.com>
2021-06-16 20:11:45 +0300

    Update notification.py

--- Notification/notification.py
+++ Notification/notification.py
@@ -4,8 +4,8 @@ from win10toast import ToastNotifier # python -m pip install win10toast
 try:
    toaster = ToastNotifier() # One-time initialization
    
-   def checkNotif():
-      toaster.show_toast(title='Notification!'title, content, threaded=True, # Show notification whenever needed
+   def Notify(title='Notification!', content):
+      toaster.show_toast(title, content, threaded=True, # Show notification whenever needed
       icon_path=None, duration=180)  # 180seconds
       
    return 0
