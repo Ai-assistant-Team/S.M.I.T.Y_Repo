29b177ae2061a944b3427a32483f16879a3a37ee
Allamir <80987177+Allamir@users.noreply.github.com>
2021-06-16 20:27:07 +0300

    Update gmailScripts_TestCases.py

--- gmailScripts/gmailScripts_TestCases.py
+++ gmailScripts/gmailScripts_TestCases.py
@@ -2,15 +2,29 @@ import read
 import send
 
 
-def test1() :
-    read.show_emails(2)
+def test1(n) :
+    read.show_emails(n)
 
 
 
-def test2() :
-    send.create_send("Hello", "dai19078@uom.edu.gr", "This is my first email")
+def test2(body="", email="", subject="") :
+    send.create_send(body, email, subject)
     
 
-test1()
+test1(2)
+#Test with number 2 which shows the latest 2 emails
 
-test2()
+test1()
+#Test where no number was specified
+
+test1(100000000)
+#Test with a really large number, I limitted it to max 20 latest emails
+
+test2("hello", "rgererge@egerg", "subject")
+#Test with an email that does not exist
+
+test2("hello", "dai19078@uom.edu.gr")
+#Test without a subject
+
+test2("hello", "dai19078@uom.edu.gr", "This is the subject")
+#Test with every parameter
