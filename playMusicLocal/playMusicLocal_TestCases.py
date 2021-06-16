a8b1df5f97129b5a93f0b8e358edd2ebeb37577d
NickKoutamanis <81077029+NickKoutamanis@users.noreply.github.com>
2021-06-16 21:35:27 +0300

    Add files via upload

---
+++ playMusicLocal/playMusicLocal_TestCases.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 21:31:40 2021
+
+@author: nickk
+"""
+
+import playMusicLocal
+
+playMusicLocal("T.N.T", "C://users/exaple/music") #Correct example
+
+playMusicLocal("C://users/exaple/music") #Only path
+
+playMusicLocal("T.N.T") #Only Name
+
+playMusicLocal() #No input
