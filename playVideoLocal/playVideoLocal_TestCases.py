7f01281501070ce144928a73bdf267d585e0f090
NickKoutamanis <81077029+NickKoutamanis@users.noreply.github.com>
2021-06-16 21:46:02 +0300

    Update playVideoLocal_TestCases.py

--- playVideoLocal/playVideoLocal_TestCases.py
+++ playVideoLocal/playVideoLocal_TestCases.py
@@ -7,10 +7,23 @@ Created on Wed Jun 16 21:25:09 2021
 
 import playVideoLocal
 
-playVideoLocal("Smells like teen spirit", "C://users/exaple/videos") #Correct example
+def testCase1():
+  playVideoLocal("Smells like teen spirit", "C://users/exaple/videos") #Correct example
 
-playVideoLocal("C://users/exaple/video") #Only path
-
-playVideoLocal("Smells like teen spirit") #Only Name
-
-playVideoLocal() #No input
+def testCase2():
+  try:
+    playVideoLocal("C://users/exaple/video") #Only path
+  except:
+    print("Error")
+    
+def testCase3():
+  try:
+    playVideoLocal("Smells like teen spirit") #Only Name
+  except:
+    print("Error")
+    
+ def testCase4():
+  try:
+    playVideoLocal() #No input
+  except:
+    print("Error")
