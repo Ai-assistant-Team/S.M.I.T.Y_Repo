213c11c27e2123dcda480fa939c3391e0c4f72e3
Allamir <80987177+Allamir@users.noreply.github.com>
2021-06-16 20:20:09 +0300

    Update forecast_weather_script_TestCase.py

--- Forecast_weather_now/forecast_weather_script_TestCase.py
+++ Forecast_weather_now/forecast_weather_script_TestCase.py
@@ -3,4 +3,13 @@ import forecast_weather_script
 def test1(name)
   forecast_weather_script.Forecast_weather_now(city=name)
   
+  
 test1("Athens")
+##Tells the weather in Athens
+
+test1()
+##null
+
+test1("TASGEDSAC")
+##City that not exists
+
