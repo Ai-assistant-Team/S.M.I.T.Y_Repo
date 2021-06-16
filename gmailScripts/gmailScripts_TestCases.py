0751444895321529f5f30997489a0e313ae5c82e
Allamir <80987177+Allamir@users.noreply.github.com>
2021-06-16 18:54:23 +0300

    Update and rename gmail.py to gmailScripts_TestCases.py

---
+++ gmailScripts/gmailScripts_TestCases.py
@@ -0,0 +1,16 @@
+import read
+import send
+
+
+def test1() :
+    read.show_emails(2)
+
+
+
+def test2() :
+    send.create_send("Hello", "dai19078@uom.edu.gr", "This is my first email")
+    
+
+test1()
+
+test2()
