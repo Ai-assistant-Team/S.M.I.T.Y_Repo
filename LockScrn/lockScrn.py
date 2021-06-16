2615547cec29f011493c3290f4983d57daf0cf09
NickKoutamanis <81077029+NickKoutamanis@users.noreply.github.com>
2021-06-16 18:51:05 +0300

    Update lockScrn.py

--- LockScrn/lockScrn.py
+++ LockScrn/lockScrn.py
@@ -8,8 +8,7 @@ def lockScrn():
 #This fnction is used to lock the device after a number of seconds speciafied by the user
 def lockScrnTimer(t): 
     import time
-    
-    
-    time.sleep(1)
+   
+    time.sleep(t)
     
     lockScrn()
