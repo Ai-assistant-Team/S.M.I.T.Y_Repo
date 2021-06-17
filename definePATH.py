9d6f48cd489d74f36a5e357448d099724bcd9f83
Teo Economides <teo.economides.vodafone@gmail.com>
2021-06-17 11:53:57 +0300

    Add files via upload

---
+++ definePATH.py
@@ -0,0 +1,7 @@
+#
+# Script created by Theodore Economides (Θεόδωρος Οικονομίδης)
+#
+
+
+## This script defines a constant that stores the path to the resources that are needed in order for other scripts to work
+RESOURCES_PATH = './Resources/'
