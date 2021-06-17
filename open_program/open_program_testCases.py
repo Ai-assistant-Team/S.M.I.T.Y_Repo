182de417fdf71a04e72cd08b9ff7b8795abd02d8
NektariosPapagalakis <79308500+NektariosPapagalakis@users.noreply.github.com>
2021-06-17 15:43:22 +0300

    Add files via upload

---
+++ open_program/open_program_testCases.py
@@ -0,0 +1,10 @@
+from open_program import *
+
+a = open_program("instagram")
+print (a)
+a = open_program("notebadd")
+print (a)
+a = open_program("notepad")
+print (a)
+a = open_program("excel")
+print (a)
