818599471ae7ff02b3cf6f7091d1e61b728af235
Teo Economides <teo.economides.vodafone@gmail.com>
2021-06-18 12:23:41 +0300

    Add files via upload

--- definePATH.py
+++ definePATH.py
@@ -3,5 +3,20 @@
 #
 
 
-## This script defines a constant that stores the path to the resources that are needed in order for other scripts to work
-RESOURCES_PATH = './Resources/'
+## This script defines constants that store the paths to the root (RESOURCES)
+## and and first level directories that contain the needed files for the other scripts to work
+import os
+
+# Get absolute path of root Folder
+root = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+
+# Root
+RESOURCES_PATH = os.path.join(root, 'Resources')
+
+# First Level Subfolders
+PATH_TO_NEWS = os.path.join(root, RESOURCES_PATH, 'News')
+PATH_TO_GUI = os.path.join(root, RESOURCES_PATH, 'GUI')
+MY_OUTPUT = os.path.join(root, PATH_TO_GUI, 'output.txt')
+PATH_TO_SETTINGS = os.path.join(root, RESOURCES_PATH, 'Settings')
+PATH_TO_ALARM_CLOCK = os.path.join(root, RESOURCES_PATH, 'AlarmClock')
+PATH_TO_CHECKLISTS = os.path.join(root, RESOURCES_PATH, 'Checklists')
