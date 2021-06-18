1abd23e006ae0bc3e5e543e2de94782cdbf03a92
FotisVlachos <81076889+FotisVlachos@users.noreply.github.com>
2021-06-18 12:00:13 +0300

    Add files via upload

--- counter/counter.py
+++ counter/counter.py
@@ -1,61 +1,54 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 25 18:50:47 2021
+import time  # library to slow the system in order to count in a specific speed
 
-@author: fotis
-"""
-
-#function to count from a number to another. The default number to start counting is 1 but the user can 
-#switch it and start counting from another number. He can also choose a speed to count. The default speed
-#is "Normal" but he can choose between "Fast" or "Slow".Any other words for speed wont be accepted.
-
-import time #library to slow the system in order to count in a specific speed
-
-def countTo(conn,to,countFrom=1,speed="Normal"):  #3 variables
+def countTo(conn, to, countFrom=1, speed="Normal"):  # 3 variables
 
     try:
-        while(countFrom<=to):      
-            if(speed=="Normal"):  #if the speed is set to "Normal" the scrip counts every one second
+        while countFrom <= to:
+            if speed == "Normal":  # if the speed is set to "Normal" the scrip counts every one second
                 conn.send(countFrom)
                 time.sleep(1)
                 countFrom += 1
-            elif(speed=="Slow"): #if the speed is set to "Slow" the scrip counts every two seconds
+            elif speed == "Slow":  # if the speed is set to "Slow" the scrip counts every two seconds
                 conn.send(countFrom)
                 time.sleep(2)
                 countFrom += 1
-            elif(speed=="Fast"): #if the speed is set to "Fast" the scrip counts every three seconds
+            elif speed == "Fast":  # if the speed is set to "Fast" the scrip counts every three seconds
                 conn.send(countFrom)
                 time.sleep(0.5)
                 countFrom += 1
+        conn.send(None)
+        conn.close()
         return 0
     except:
+        conn.close()
         return 1
-    
-  
-#function to count down from a number to 1. The default number to count down to is 1 but the user can 
-#switch it and start counting down to another number. He can also choose a speed to count down. The default speed
-#is "Normal" but he can choose between "Fast" or "Slow".Any other words for speed wont be accepted.
-            
-def countDown(conn,countDownFrom,countDownTo=1,speed="Normal"):
 
+
+# function to count down from a number to 1. The default number to count down to is 1 but the user can
+# switch it and start counting down to another number. He can also choose a speed to count down. The default speed
+# is "Normal" but he can choose between "Fast" or "Slow".Any other words for speed wont be accepted.
+
+def countDown(conn, countDownFrom, countDownTo=1, speed="Normal"):
     try:
-        while(countDownFrom>=countDownTo):
-                if(speed=="Normal"):        #if the speed is set to "Normal" the scrip counts down every one second
-                    conn.send(countDownFrom)
-                    time.sleep(1)
-                    countDownFrom -= 1
-                elif(speed=="Slow"):        #if the speed is set to "Slow" the scrip counts down every two seconds
-                    conn.send(countDownFrom)
-                    time.sleep(2)
-                    countDownFrom -= 1
-                elif(speed=="Fast"):        ##if the speed is set to "Fast" the scrip counts every three seconds
-                    conn.send(countDownFrom)
-                    time.sleep(0.5)
-                    countDownFrom -= 1
-        return 0 
+        while countDownFrom >= countDownTo:
+            if speed == "Normal":  # if the speed is set to "Normal" the scrip counts down every one second
+                conn.send(countDownFrom)
+                time.sleep(1)
+                countDownFrom -= 1
+            elif speed == "Slow":  # if the speed is set to "Slow" the scrip counts down every two seconds
+                conn.send(countDownFrom)
+                time.sleep(2)
+                countDownFrom -= 1
+            elif speed == "Fast":  ## if the speed is set to "Fast" the scrip counts every three seconds
+                conn.send(countDownFrom)
+                time.sleep(0.5)
+                countDownFrom -= 1
+        conn.send(None)
+        conn.close()
+        return 0
     except:
+        conn.close()
         return 1
-           
-            
+
 
 
