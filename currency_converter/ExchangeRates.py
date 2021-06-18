8bd9ec1c3a66b105ec4b8e444e357b0c72b4cb5e
dsoump <80273571+dsoump@users.noreply.github.com>
2021-06-18 18:42:46 +0300

    Update ExchangeRates.py

--- currency_converter/ExchangeRates.py
+++ currency_converter/ExchangeRates.py
@@ -6,13 +6,15 @@ Created on Sat May 8
 from forex_python.converter import CurrencyRates
 
 c = CurrencyRates()
-#c1 = 'USD'  #Currency you are converting from 
-#c2 = 'EUR'  #Currency you are converting to
+#c1 = 'USD'  Currency you are converting from 
+#c3= 'EUR'  Currency you are converting to
+#c2= 'Euros'         c2 = Full name of the currency that the user wants to conver to 
 #amount= the amount you are converting
 
 def cconvert(c1, c2, amount):       #Converts the amount from currency 1 to currency 2
     try:
         fin=(c.convert(c1, c2, amount)) 
+        fin= str(fin) + ' ' + c2
         return fin
     except:
         return 1
