7373dcff97f207958f602a813ca2ef8b190ebd08
NektariosPapagalakis <79308500+NektariosPapagalakis@users.noreply.github.com>
2021-06-19 12:50:33 +0300

    Update calendar.py

--- Calendar/calendar.py
+++ Calendar/calendar.py
@@ -1,45 +1,40 @@
+#import libraries
 import datetime
 import pathlib
 
+#register an event to calendar_record
 def make_event(date,time,description):
     try:
+        # get the date of the event
         date = get_date(date)
+        #if there is something wrong with the date return this message
         if date.__contains__('of the event must in the present or in the future not in the past YOU IDIOT'):
             return date
+        # get the time of the event
         time = get_time(time,date)
+        #if there is something wrong with the time return this message
         if time.__contains__('of the event must be between') or time.__contains__('Unless you time travel you can not schedule something in the past'):
             return time
+        # register the date to the calendar_record.txt
         put_on_record(date)
+        # register the time to the calendar_record.txt
         put_on_record(time)
+        # register the description to the calendar_record.txt
         get_description(description)
         return 0
     except:
+        #if something gose wrong return error code
         return 1
-
-
-def turn_text_to_date(users_input):
-    try:
-        #todays date
-        todays_date_and_hour = datetime.datetime.now()
-        date = [0,0,0000]
-        date[0] = todays_date_and_hour.day
-        date[1] = todays_date_and_hour.month
-        date[2] = todays_date_and_hour.year
-
-        if users_input == 'tomorrow':
-            date[0] = date[0] +1
-            return '/'.join([str(elem) for elem in date])
-        elif users_input == 'the day after tomorrow':
-            date[0] = date[0] +2
-            return '/'.join([str(elem) for elem in date])
-        return users_input
-    except:
-        return 1
+    
 
 def show_me(date):
+    #return events in one date
     try:
+        #get scripts location
         location = pathlib.Path(__file__).parent.absolute()
+        #initialize events_found
         events_found =''
+        #open nad read calendar_record.txt
         f = open("%s\\calendar_record.txt"%(location), "r")
         while 5>4:
             line = f.readline()
@@ -47,29 +42,46 @@ def show_me(date):
                 break
             temp = date + '\n'
             if line == temp:
+                #remove "\n"
                 events_found = events_found + line.replace("\n", " ")
+                #read line
                 line = f.readline()
+                #remove "\n"
                 events_found = events_found + line.replace("\n", " ")
+                #read line
                 line = f.readline()
+                #read line
+                #add line
                 events_found = events_found + line
         f.close()
+        #return the events that match the date
         return events_found
     except :
         return 8
 
 def show_me_in_range(date1,date2):
+#return events in a range of dates
     try:
+        #initialize events_found
         events_found =''
         while 5>4:
+            #while true
             a =show_me(date1)
+            #get events for one date
+            #if something went wrong return error code
             if a == 1:
                 return 8
+            #add the events of the resered date to the totall
             events_found = events_found +a
+            #if something went wrong return error code
             if date1 == date2:
                 break
             date1 = next_day(date1)
+            #go to next day
             if date1 == 1:
+                #if something went wrong return error code
                 return 1
+         #return events found
         return events_found
     except:
         return 1
@@ -77,9 +89,16 @@ def show_me_in_range(date1,date2):
             
 def next_day (date):
     try:
+        #split date at "/"
         date = date.split("/")
+        #turn date[0] form str to int
+        #day
         date[0] = int (date[0])
+        #turn date[1] form str to int
+        #month
         date[1] = int (date[1])
+        #turn date[2] form str to int
+        #year
         date[2] = int (date[2])
 
         if date[0] < 28  :
