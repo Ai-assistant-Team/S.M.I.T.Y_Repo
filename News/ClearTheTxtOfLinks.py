ab4d8d242a59240056be2d3f8fb40fe8d883f366
NektariosPapagalakis <79308500+NektariosPapagalakis@users.noreply.github.com>
2021-06-19 15:54:31 +0300

    Update ClearTheTxtOfLinks.py

--- News/ClearTheTxtOfLinks.py
+++ News/ClearTheTxtOfLinks.py
@@ -62,24 +62,31 @@ def update_txt(list_of_links):
 
 
 def clear_the_txt_of_links():
+    #clear links.txt from the unwanted links
     #open file
     try:
+        #find links.txt
         location = os.path.join(RESOURCES_PATH, DIRNAME, record_file)
+        #open links.txt
         file = open(location,'r')
-
+        # initialize lists
         list_of_links = []
         new_list_of_links = []
         new_list_of_links2 = []
-
+        #call get_list_from_txt 
         ex = get_list_from_txt(list_of_links)
+        #if get_list_from_txt returns 8 return 8
         if ex == 8:
             return 8
         # Closing file
         file.close()
         #check for double links
         for link in list_of_links:
+            #set for every link count = 0
             count = 0
+             #put link in a
             a = link
+            #run the lists
             for new_link in new_list_of_links:
                 b = new_link
                 if a == b :
@@ -96,6 +103,7 @@ def clear_the_txt_of_links():
                 new_list_of_links2.append(a)
         #open file
         file = open(location,'w')
+        #write to file
         file.write(str(len(new_list_of_links2)))
         file.write('\n')
         file.write('0\n')
@@ -105,17 +113,24 @@ def clear_the_txt_of_links():
             file.write(link)
         # Closing file
         file.close
+         #if all good sed 0
         return 0
     except:
+        #return 9 if there is a problem writing the file
         return 9
 
 
 def get_list_from_txt(list_of_links):
+    #creates a list of links fom the txt file
     try:
+        #find links.txt
         location = os.path.join(RESOURCES_PATH, DIRNAME, record_file)
+        #open links.txt
         file = open(location,'r')
         for line in file:
+            #add every line of the txt to the list
             list_of_links.append(line)
         return list_of_links
     except:
         return 8
+        #return 8 if there is a problem reading the file
