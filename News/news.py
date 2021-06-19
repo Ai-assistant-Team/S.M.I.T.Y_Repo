d86a79f8ed5ef375c38f5a5e4ce58be7d46551d0
NektariosPapagalakis <79308500+NektariosPapagalakis@users.noreply.github.com>
2021-06-19 16:07:54 +0300

    Update news.py

--- News/news.py
+++ News/news.py
@@ -27,31 +27,43 @@ def get_news():
         end = int(list_of_links[3])
 
         if(used < totall):
+            #if the links that the user has already seen is less than the totall news
             if (used + 5 < totall):
+                #if the links that the user has already seen plush the 5 links that is going to see are less than the totall news
                 step = 5
             else:
+                #if the remaining links are less than 5
                 step = totall-used
 
             for x in range(start,end):
+                #turn links in to tiltles
                 title_article =  get_title_from_article(list_of_links[x])
                 if title_article == 10:
                     return 10
                 news_titles = news_titles + title_article + '\n'
-
+            #increas the used links
             used = used +step
+            #increas the line of the first link
             start = start +step
+            #increas the line of the last link
             end = end +step
+            #write the rest links number
             list_of_links[1] = str(used) + '\n'
+            #write the next first link number
             list_of_links[2] = str(start) + '\n'
+            #write the next last link number
             list_of_links[3] = str(end) + '\n'
+            #call update_txt(list_of_links)
             a = update_txt(list_of_links)
             if a == 9:
+                #return 9 if there is a problem writing the file
                 return 9
         else:
+            #if there is no more titles aveliable
             news_titles = "No more news aveliable"
         return news_titles
     except:
-        return 1
+        return 1  #return 9 if something went wrong
 
 def open_news_page(title):
     try:
