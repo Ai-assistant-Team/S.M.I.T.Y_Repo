2e96eebaeace014e189f457846b7bbe2e98cd866
NickKoutamanis <81077029+NickKoutamanis@users.noreply.github.com>
2021-06-19 18:09:37 +0300

    Add files via upload

--- playVideoLocal/playVideoLocal.py
+++ playVideoLocal/playVideoLocal.py
@@ -5,16 +5,16 @@ Created on Sun Apr  4 14:58:13 2021
 @author: nickk
 """
 
-def playVideoLocal(videoName, videoPath): #Given a name and a path plays a video file on the system defaul player
+def playVideoLocal(videoname, videopath): #Given a name and a path plays a video file on the system defaul player
     try:
         import os #Required to start the video file
         import sys #Required to stop the script if video file is not found
 
     
-        videoTypes = ['.m4v','.mp4','.mov','.asf','.avi','.wmv','.m2ts','.3g2','.3gp2','.3gpp'] #The video types supported by the script
+        videotypes = ['.m4v','.mp4','.mov','.asf','.avi','.wmv','.m2ts','.3g2','.3gp2','.3gpp'] #The video types supported by the script
     
         for i in range(6):
-            result = find(videoName+videoTypes[i], videoPath) #Searches for the video file with every one of the above extentions
+            result = find(videoname+videotypes[i], videopath) #Searches for the video file with every one of the above extentions
             if result != None: #if the video file is found break the loop
                 break
         
@@ -28,11 +28,11 @@ def playVideoLocal(videoName, videoPath): #Given a name and a path plays a video
     except:
         return 1
     
-def find(fileName, filePath): #Searches for the video file in a given path and if found returns full path
+def find(filename, filepath): #Searches for the video file in a given path and if found returns full path
     try:
         import os
-        for root, dirs, files in os.walk(filePath):
-            if fileName in files:
-                return os.path.join(root, fileName)
+        for root, dirs, files in os.walk(filepath):
+            if filename in files:
+                return os.path.join(root, filename)
     except:
         return 1
--- playVideoLocal/playVideoLocalTest.py
+++ playVideoLocal/playVideoLocalTest.py
@@ -10,26 +10,26 @@ def test():
     import os #Required to see the contents of a txt file
     import sys #Required to stop the script
     
-    vName = input("Insert video name: ")
+    vname = input('Insert video name: ')
+    filename = 'pathfilev.txt'
+    pathfilew = open(filename, 'a+')
     
-    pathfileW = open("pathfileV.txt", "a+")
-    
-    if(os.stat("pathfileV.txt").st_size == 0): #if txt is empty prompt the user to select a search location
+    if(os.stat(filename).st_size == 0): #if txt is empty prompt the user to select a search location
         import tkinter as tk
         from tkinter.filedialog import askdirectory
         window = tk.Tk()
         path = askdirectory(title='Select Video Folder')
         window.withdraw()
-        pathfileW.write(path)
+        pathfilew.write(path)
     else: #else read the location from the txt
-        pathfileR = open("pathfileV.txt", "r")
-        path = pathfileR.readline()
+        pathfiler = open(filename, 'r')
+        path = pathfiler.readline()
         print(path)
-        pathfileR.close()
+        pathfiler.close()
         
     
-    pathfileW.close()
-    playVideoLocal(vName, path) #Play the audio file with the given name in thee given location
+    pathfilew.close()
+    playVideoLocal(vname, path) #Play the audio file with the given name in thee given location
     sys.exit()
     
 test()
