0244cce2cc0bdeee1cd70dcc2c0b5d93c7a56f31
Teo Economides <teo.economides.vodafone@gmail.com>
2021-06-21 15:21:36 +0300

    Add files via upload

--- funFactsScript/funFacts.py
+++ funFactsScript/funFacts.py
@@ -4,11 +4,11 @@ Created on Sat Mar 27 13:08:39 2021
 @author: manolispolymeneris
 """
 
-import randfacts #Imports random fun fact generator library
 
 def funFacts():
     
     try:
+        import randfacts #Imports random fun fact generator library
         for i in range(5142): #Runs the list with fun facts
             fact = randfacts.getFact() #Generates a fun fact
             return fact #Returns and prints the fun fact
