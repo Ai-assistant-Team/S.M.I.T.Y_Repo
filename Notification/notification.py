c68274028f8ca76ecc2f136e3771816ec0529495
Tasos Papadopoulos <80987177+Allamir@users.noreply.github.com>
2021-06-23 00:57:04 +0300

    Add files via upload

--- Notification/notification.py
+++ Notification/notification.py
@@ -3,18 +3,19 @@ Created on We Jun 16 20:16:19 2021
 @author: manolispolymeneris
 """
 
-from win10toast import ToastNotifier # python -m pip install win10toast
+from win10toast import ToastNotifier  # python -m pip install win10toast
+
+toaster = ToastNotifier()  # One-time initialization
 
 
-   toaster = ToastNotifier() # One-time initialization
-   
-   def notify(content, title='Notification!'):
-      try:
-         toaster.show_toast(title, content, threaded=True, # Show notification whenever needed
-         icon_path=None, duration=180)  # 180 seconds
-      
-         return 0
-      
-      except:
-         
-         return 1
+def notify(content, title='Notification!'):
+    try:
+
+        toaster.show_toast(title, content, threaded=True,  # Show notification whenever needed
+                           icon_path=None, duration=180)  # 180 seconds
+
+        return 0
+
+    except:
+
+        return 1
