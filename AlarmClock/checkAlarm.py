c82d6a6485d60bac99fc0b6cb1564ec78560a3f2
Tasos Papadopoulos <80987177+Allamir@users.noreply.github.com>
2021-06-23 00:56:27 +0300

    Add files via upload

--- AlarmClock/checkAlarm.py
+++ AlarmClock/checkAlarm.py
@@ -5,68 +5,78 @@ Created on Fri Jun 18
 import datetime
 import os
 import time
-file = "hours.txt"
+from SMITY.definePATH import PATH_TO_ALARM_CLOCK
+from win10toast import ToastNotifier
+
+file = 'hours.txt'
+
 
 def checkTime():
     try:
-        while True: #running forever
-            
-            if os.path.exists(file) : #checks if the file hours.txt exists
-                
-                if os.stat(file).st_size > 0 : #checks  if the file hours.txt is empty
-                    
-                    with open(file, "r") as f: #if it exists it opens the file
+        while True:  # running forever
 
-                        a = [a.strip() for a in f]  #formats the content of the txt file to a list of integers that represent the time
+            if os.path.exists(os.path.join(PATH_TO_ALARM_CLOCK, file)):  # checks if the file hours.txt exists
 
-                    s = [] #new list
+                if os.stat(
+                        os.path.join(PATH_TO_ALARM_CLOCK, file)).st_size > 0:  # checks  if the file hours.txt is empty
+
+                    with open(os.path.join(PATH_TO_ALARM_CLOCK, file), 'r') as f:  # if it exists it opens the file
+
+                        a = [a.strip() for a in
+                             f]  # formats the content of the txt file to a list of integers that represent the time
+
+                    s = []  # new list
 
                     for x in a:
-                        s.append(int(x) - datetime.datetime.now().hour) #puts each element of list 'a' to list 's' where every element is the result of its self minus the current time
+                        s.append(int(
+                            x) - datetime.datetime.now().hour)  # puts each element of list 'a' to list 's' where
+                        # every element is the result of its self minus the current time
 
                     minv = 24
 
                     for x in s:
 
-                        if int(x) < minv and int(x) >= 0: #finds the element that is the lowest and not negative
+                        if int(x) < minv and int(x) >= 0:  # finds the element that is the lowest and not negative
                             minv = int(x)
 
                     p = []
-                    
+
                     j = 0
-                    
+
                     k = 0
-                    
+
                     for x in s:
 
                         if int(x) != 0:
-                            p.append(x) #puts all elements into a new list excluding elements that are equal to zero
-                            
+                            p.append(x)  # puts all elements into a new list excluding elements that are equal to zero
+
                             k += 1
-                            
+
                         else:
                             j += 1
 
                     y = 0
 
                     for x in p:
-                        
-                        p[y] = int(x) + datetime.datetime.now().hour #converts each element back to its original state by additive its self and current time
-                        
+                        p[y] = int(
+                            x) + datetime.datetime.now().hour  # converts each element back to its original state by
+                        # additive its self and current time
+
                         y += 1
 
-                    
                     b = 0
-                    
-                    if (minv == 0): #if there was an element equal to 0 and so is min it means that the ring has to go off
-                        
-                        wakeup="WAKE UP!"
-                        
-                        
-                        with open(file, "w") as txt_file: #saving every element from list p to the hours.txt file
-                            
+
+                    if (
+                            minv == 0):  # if there was an element equal to 0 and so is min it means that the ring
+                        # has to go off
+
+                        wakeup = 'WAKE UP!'
+
+                        with open(os.path.join(PATH_TO_ALARM_CLOCK, file),
+                                  'w') as txt_file:  # saving every element from list p to the hours.txt file
+
                             for line in p:
-                                
+
                                 if b == 0:
 
                                     txt_file.write(str(line))
@@ -74,34 +84,45 @@ def checkTime():
                                 else:
 
                                     txt_file.write("\n" + "".join(str(line)))
-                                    
+
                                 b += 1
-                                
-                                if b == (k - j): #the size of the list minus the number of all the elements that where 0
-                                    
+
+                                if b == (
+                                        k - j):  # the size of the list minus the number of all the elements that
+                                    # where 0
+
                                     txt_file.close()
-                                    
+
                                     f.close()
-                                    
-                                    return wakeup
-                                
-                                    break
+
+                                    notify(wakeup)
 
                         txt_file.close()
-                        
+
                     f.close()
-                    
+
                     time.sleep(2)
                 else:
 
-                    time.sleep(10) #checks if the file is empty
+                    time.sleep(10)  # checks if the file is empty
             else:
 
-                time.sleep(10) #checks if the file exists
-            #end of while
+                time.sleep(10)  # checks if the file exists
+            # end of while
 
-    except OSError :
-        return 8 #It could not open and read the file
+    except OSError:
+        return 8  # It could not open and read the file
 
 
 checkTime()
+def notify(content, title='Notification!'):
+    try:
+        toaster = ToastNotifier()  # One-time initialization
+        toaster.show_toast(title, content, threaded=True,  # Show notification whenever needed
+                           icon_path=None, duration=180)  # 180 seconds
+
+        return 0
+
+    except:
+
+        return 1
