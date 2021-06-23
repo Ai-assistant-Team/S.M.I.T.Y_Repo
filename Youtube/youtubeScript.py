b238bc8ff1a908b6e4157b04ba5bf0ba33e77731
Teo Economides <teo.economides.vodafone@gmail.com>
2021-06-23 20:25:21 +0300

    Add files via upload

--- Youtube/youtubeScript.py
+++ Youtube/youtubeScript.py
@@ -1,13 +1,16 @@
-import webbrowser                                                                                  # importing webbrowser module
+import SMITY.urlHandling.openUrls
 
 ##
 ## This function is searching youtube the video you want
 ##
 
-def Youtube_Search(toSearch):                                                                           # declaring a function
-        try:                                                                                            # using Exception Handling to avoid unprecedented errors
-            return(webbrowser.open('https://www.youtube.com/results?search_query=' + toSearch))         # it searches a youtube video of what you typed
-        except Exception :
-            return 1                                                                                    # 1 : Undefined Error
 
-#end of youtubeScript
+def Youtube_Search(toSearch):  # declaring a function
+    try:  # using Exception Handling to avoid unprecedented errors
+
+        return SMITY.urlHandling.openUrls.openUrl('https://www.youtube.com/results?search_query=' + toSearch)  # it searches a youtube video of what you typed
+
+    except Exception:
+        return 1  # 1 : Undefined Error
+
+# end of youtubeScript
