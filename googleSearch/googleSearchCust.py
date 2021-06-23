7a8ebc3fc3722664dc378eae07f7591f604b451e
Teo Economides <teo.economides.vodafone@gmail.com>
2021-06-23 14:11:43 +0300

    Add files via upload

--- googleSearch/googleSearchCust.py
+++ googleSearch/googleSearchCust.py
@@ -5,12 +5,13 @@ from googlesearch import search       # import the parts of the googlesearch tha
 
 from urllib.request import urlopen
 
+
 def searching(query=''):
     try:
         for i in search(query, num_results=3):    #By default, googlesearch returns 10 results,this can be changed by using the variable num_reseult
             print(i)                 # printing top three results
         
-            return 0
+        return 0
 
     except:
         if internet_on():
