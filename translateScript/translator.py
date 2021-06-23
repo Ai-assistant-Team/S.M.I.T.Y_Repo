b2a895dba335132afe5f9464ccc2239519b93ce8
Teo Economides <teo.economides.vodafone@gmail.com>
2021-06-23 18:25:48 +0300

    Add files via upload

--- translateScript/translator.py
+++ translateScript/translator.py
@@ -1,17 +1,17 @@
-#import the needed libraries
+# import the needed libraries
 from googletrans import Translator
 
-#function that translates a given text to the given language
-def translateText(textToTranslate,lang):
 
+# function that translates a given text to the given language
+def translateText(textToTranslate, lang):
     try:
-        #Google Translate ajax API implementation class
+        # Google Translate ajax API implementation class
         translator = Translator()
-    
-        #Translate text from source language to destination language
-        translated = translator.translate(textToTranslate,dest=lang)
-   
-        #Show the translated text on screen
-        return print(translated.text)
+
+        # Translate text from source language to destination language
+        translated = translator.translate(textToTranslate, dest=lang)
+
+        # Show the translated text on screen
+        return translated.text
     except:
         return 1
